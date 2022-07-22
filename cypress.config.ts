8a66529bca0e1fe464543baeed2d7fd09837b5ff
Jiadong.Chen <coolboy096188@gmail.com>
2022-07-22 18:23:12 +0900

    add report

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from "cypress"
+import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+  },
+  reporter: "mochawesome",
+  reporterOptions: {
+    overwrite: false,
+    html: false,
+    json: true,
   },
 });
